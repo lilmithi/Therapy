d645cf82108adabc9a30763f5a60385dffb2660a
martin mithi <gathitumithi9000@gmail.com>
2023-03-24 07:35:37 +0300

    Change file extension

--- src/App.js
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <div className="App text-green-500">Hello</div>;
-}
-
-export default App;
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import LandingPage from "./pages/landing/LandingPage";
+
+function App() {
+  return (
+    <div className="App 2xl:container mx-auto font-NunitoSans">
+      <LandingPage />
+    </div>
+  );
+}
+
+export default App;
